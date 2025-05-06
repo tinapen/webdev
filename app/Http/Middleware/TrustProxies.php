69c98d8cb974f6156e8fd7178963076b042920fe
Tinapen <cristinapenaflor20@gmail.com>
2025-05-06 19:52:12 +0800

    updates

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,15 +12,15 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
-
+    // protected $proxies;
+    protected $proxies = '*';
     /**
      * The headers that should be used to detect proxies.
      *
      * @var int
      */
     protected $headers =
-        Request::HEADER_X_FORWARDED_FOR |
+    Request::HEADER_X_FORWARDED_FOR |
         Request::HEADER_X_FORWARDED_HOST |
         Request::HEADER_X_FORWARDED_PORT |
         Request::HEADER_X_FORWARDED_PROTO |
